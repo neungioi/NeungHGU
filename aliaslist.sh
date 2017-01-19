c09101d67f82b7c2a86cf3ab2d5b04803043dcdd
neungioi <rnjssmd6043@gmail.com>
2017-01-19 16:38:41 +0900

    test commit

---
+++ aliaslist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPTPATH=$home/home/neungioi/data/program
+
+alias execute="ipython $SCRIPTPATH/path1.py $1; ipython $SCRIPTPATH/path2.py $1; ipython $SCRIPTPATH/path2_2.py $1; ipython $SCRIPTPATH/path3.py $1"
